9ffa410f98723e75f67a1928204416cb5770644a
alexsilva75 <alexsandro.ssantos75@gmail.com>
2021-10-12 08:49:32 -0300

    Portal CR Telecom - Primeiro commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CR Market</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+
+</head>
+
+<body>
+    <div class="hero-container">
+        <div class="container">
+            <section class="hero-section">
+                <div class="left-col">
+                    <h1>Qual é a boa?</h1>
+
+                    <div class="hero-feature-box buy-box">
+
+                    </div>
+                    <div class="hero-feature-box sell-box">
+                        
+                    </div>
+                </div>
+                <div class="right-col">
+
+                </div>
+            </section>
+        </div>
+        
+    </div>
+
+</body>
+
+</html>
