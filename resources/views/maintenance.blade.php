df5105fda11ef05e85fe408cf52bb762147256b7
alexsilva75 <alexsandro.ssantos75@gmail.com>
2021-10-22 12:20:54 -0300

    Animate logo maintenance fix 2

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/general.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/queries.css') }}">
     <title>CR Telecom &mdash; Portal</title>
 </head>
 
