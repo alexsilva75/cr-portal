6063477915b19d6282b80b68e7993c5b7afdc4d4
alexsilva75 <alexsandro.ssantos75@gmail.com>
2021-10-22 12:12:09 -0300

    Animate logo maintenance

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -5,11 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/general.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>CR Telecom &mdash; Portal</title>
 </head>
 
-<body>
-    <h1>Em breve, você conhecerá o nosso novo portal. Aguarde!</h1>
+<body onload="animateLogo()">
+    <main>
+        <section class="maintenance">
+            <div class="container">
+                <h1 class="heading-primary">Em breve, você conhecerá o nosso novo portal. Aguarde!</h1>
+                <div class="logo-container logo-base">
+                    <img src="{{ asset('img/logo.png') }}" alt="" class="logo-lg">
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <script>
+        function animateLogo() {
+            let logoContainerEl = document.querySelector('.logo-container');
+            logoContainerEl.classList.add('logo-appear');
+        }
+    </script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('maintenance');
 });
+
+
+Route::get('/preview', function () {
+    return view('welcome');
+});
