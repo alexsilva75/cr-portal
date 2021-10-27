2a17a41edf30e9eec14f8aa2f25d2c3ae1f1c368
alexsilva75 <alexsandro.ssantos75@gmail.com>
2021-10-27 10:52:54 -0300

    Responsivity tablets and desktops fix04

--- public/js/script.js
+++ public/js/script.js
@@ -1,50 +1,68 @@
 // alert('hi')
 
-const headerEl = document.querySelector('.header');
+const headerEl = document.querySelector(".header");
 
-function toggleDrawer(){
-    headerEl.classList.toggle('nav-open');
+function toggleDrawer() {
+    headerEl.classList.toggle("nav-open");
 }
 
-const btnMobileNav = document.querySelector('.btn-mobile-nav');
+const btnMobileNav = document.querySelector(".btn-mobile-nav");
 
-btnMobileNav.addEventListener('click', toggleDrawer);
+btnMobileNav.addEventListener("click", toggleDrawer);
 
-function updateCopyrightYear(){
-    let yearEl = document.querySelector('.year');
+function updateCopyrightYear() {
+    let yearEl = document.querySelector(".year");
     yearEl.textContent = new Date().getFullYear();
 }
 
 updateCopyrightYear();
 
-let allLinks = document.querySelectorAll('a:link');
+let allLinks = document.querySelectorAll("a:link");
 
-allLinks.forEach(linkEl => {
-    linkEl.addEventListener('click', function(event){
+allLinks.forEach((linkEl) => {
+    linkEl.addEventListener("click", function (event) {
         event.preventDefault();
 
-        const hrefAttr = linkEl.getAttribute('href');
+        const hrefAttr = linkEl.getAttribute("href");
 
-        if(hrefAttr === '#'){
+        if (hrefAttr === "#") {
             window.scrollTo({
                 top: 0,
-                behavior: 'smooth',
+                behavior: "smooth",
             });
         }
-    
-        if(hrefAttr != '#' && hrefAttr.startsWith('#')){
+
+        if (hrefAttr != "#" && hrefAttr.startsWith("#")) {
             const sectionEl = document.querySelector(hrefAttr);
-            sectionEl.scrollIntoView({behavior: 'smooth'})
+            sectionEl.scrollIntoView({ behavior: "smooth" });
         }
-    
-    
-        if(linkEl.classList.contains('main-nav-link')){
+
+        if (linkEl.classList.contains("main-nav-link")) {
             // alert('remove')
-            headerEl.classList.remove('nav-open');
+            headerEl.classList.remove("nav-open");
+        }
+    });
+});
+
+const sectionHeroEl = document.querySelector(".hero-container");
+
+const observer = new IntersectionObserver(
+    function (entries, observer) {
+        const ent = entries[0];
+        if (ent.isIntersecting === false) {
+            document.body.classList.add("sticky");
         }
 
+        if (ent.isIntersecting === true) {
+            document.body.classList.remove("sticky");
+        }
+    },
 
-    });
+    {
+        root: null,
+        threshold: 0,
+        rootMargin: "-84px",
+    }
+);
 
-    
-})
+observer.observe(sectionHeroEl);
