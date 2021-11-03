d3619d48d524303889c7b6b795fe34751c6d3d21
alexsilva75 <alexsandro.ssantos75@gmail.com>
2021-11-03 14:19:49 -0300

    Sending mail fix01

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -284,7 +284,7 @@
 
                     <p class="subheading">Dúvidas?</p>
                     <h2 class="heading-secondary">Fale Conosco!</h2>
-                    <form action="{{ route('send') }}">
+                    <form method="POST" action="{{ route('send') }}">
                         <div class="form-group">
                             <label for="nome" class="input-caption">Nome*</label>
                             <input type="text" name="nome" id="nome" class="form-control" required>
