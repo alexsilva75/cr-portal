71e08170dd7e6edd4614e51b578cb8d173aae2f3
alexsilva75 <alexsandro.ssantos75@gmail.com>
2021-11-06 09:52:13 -0300

    Sending mail fix13

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -22,10 +22,11 @@ class MailController extends Controller
 
         if (mail(
             'alexsandro.silva77@outlook.com',
-            'Formulário de contato - Portal CR Telecom',
-            "{$data['nome']} \n {$data['email']} \n {$data['fone']} \n {$data['mensagem']}",
-            [],
-            ""
+            '=?utf-8?B?' . base64_encode('Formulário de contato - Portal CR Telecom') . '?=',
+            utf8_encode("{$data['nome']} \n {$data['email']} \n {$data['fone']} \n {$data['mensagem']}"),
+            'From: form-contato@portalcrtelecom.com.br' . "\r\n"
+                . 'Content-Type: text/plain; charset=utf-8' . "\r\n",
+
         )) {
             return response()->json([
                 'success' => 'Sua mensagem foi enviada com sucesso e está sendo processada. Em breve, entraremos em contato para respondê-la.', 200
