10fdb45495b7023c23329ea59b2042d7a172e93e
alexsilva75 <alexsandro.ssantos75@gmail.com>
2021-12-15 15:35:41 -0300

    fixing mail recipient

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -21,7 +21,7 @@ class MailController extends Controller
 
 
         if (mail(
-            'alexsandro.silva77@outlook.com',
+            'contato@portalcrtelecom.com.br',
             '=?utf-8?B?' . base64_encode('Formulário de contato - Portal CR Telecom') . '?=',
             "{$data['nome']} \n {$data['email']} \n {$data['fone']} \n {$data['mensagem']}",
             'From: form-contato@portalcrtelecom.com.br' . "\r\n"
