62790d4410a8424d9457d6d093e95d7ba3b10ad8
Alexsandro Silva Santos <alexsilva75>
2022-05-29 21:31:43 -0300

    Test websocket 06

--- public/js/cr-chat.js
+++ public/js/cr-chat.js
@@ -13,7 +13,7 @@ function sendWSCRChatMessage(e) {
 
     if (!socket) {
         console.log("Creating the socket");
-        socket = new WebSocket("wss://portalcrtelecom.com.br:8090");
+        socket = new WebSocket("wss://portalcrtelecom.com.br:9443");
         socket.addEventListener("open", function (event) {
             console.log("Opening the connection");
             socket.send(JSON.stringify(createMessage("customer", "opening")));
