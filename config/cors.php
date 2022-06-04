21ac3dd511efdd09c886e99443bf78944efad275
Alexsandro Silva Santos <alexsilva75>
2022-06-03 22:07:07 -0300

    Fixing sanctum auth

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://ws.portalcrtelecom.com.br'],
 
     'allowed_origins_patterns' => [],
 
